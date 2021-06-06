22559b11fe6fc55968a4d1460e4cc78d88c42dc2
Eduardo Fillipe da Silva Reis <eduardo556@live.com>
2021-06-06 19:00:22 -0300

    Desabilitando o CORS.

--- src/main/java/br/ufs/hu/prevsep/web/api/config/BeanConfig.java
+++ src/main/java/br/ufs/hu/prevsep/web/api/config/BeanConfig.java
@@ -32,16 +32,15 @@ public class BeanConfig {
         return new BCryptPasswordEncoder();
     }
 
-        @Bean
-        public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurerAdapter() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-                }
-            };
-        }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");            }
+        };
+    }
 
     @Bean
     public AccessDecisionManager accessDecisionManager() {
