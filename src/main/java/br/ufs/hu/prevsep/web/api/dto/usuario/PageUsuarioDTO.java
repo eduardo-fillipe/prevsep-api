8d62e985fe002fc4fb5bcb2eb48bc679c0d62de8
Eduardo Fillipe da Silva Reis <eduardo556@live.com>
2021-06-12 22:36:37 -0300

    Pageables alterados para utilizar as ferramentas do Spring.

--- src/main/java/br/ufs/hu/prevsep/web/api/dto/usuario/PageUsuarioDTO.java
+++ src/main/java/br/ufs/hu/prevsep/web/api/dto/usuario/PageUsuarioDTO.java
@@ -1,13 +1,14 @@
 package br.ufs.hu.prevsep.web.api.dto.usuario;
 
 import br.ufs.hu.prevsep.web.api.dto.PageResponse;
+import org.springframework.data.domain.Page;
 
 public class PageUsuarioDTO extends PageResponse<UsuarioDTO> {
-    public PageUsuarioDTO(PageResponse<UsuarioDTO> pageResponse) {
-        super(pageResponse);
+    public PageUsuarioDTO(Page<UsuarioDTO> page) {
+        super(page);
     }
 
-    public static PageUsuarioDTO of(PageResponse<UsuarioDTO> pageResponse) {
-        return new PageUsuarioDTO(pageResponse);
+    public static PageUsuarioDTO of(Page<UsuarioDTO> page) {
+        return new PageUsuarioDTO(page);
     }
 }
